858c1e25ea042d5920d1d8ca50150f11715ceb3a
Martin070106 <kameczmartin@gmail.com>
2025-11-28 08:09:22 +0100

    elso commit

---
+++ UserRoleApi/Models/UserRoleDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserRoleApi.Models
+{
+    public class UserRoleDbContext : DbContext
+    {
+        public UserRoleDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<User> users { get; set; }
+        public DbSet<Role> roles { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost; database=userroles; user=root; password=");
+        }
+    }
+}
